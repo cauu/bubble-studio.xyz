7e20993e6a1f4b85447529304dc8286d7d9a0c46
caiyufu <cauu@163.com>
2024-11-11 23:40:13 +0800

    feat: nav bar

--- src/components/NavBar/Item.tsx
+++ src/components/NavBar/Item.tsx
@@ -2,14 +2,20 @@ import classNames from 'classnames';
 
 interface IProps {
   active?: boolean;
+  text: string;
+  href?: string;
 }
 
-export const NavBarItem = ({ active }: IProps) => {
+export const NavBarItem = ({ active, text, href }: IProps) => {
   const className = classNames(
-    'leading-8 w-20 flex items-center justify-center cursor-pointer z-10 transition-all duration-300 text-white hover:-translate-y-0.5',
-    active ? 'text-blue-500' : '',
+    'leading-8 w-20 flex items-center justify-center cursor-pointer z-10 transition-all duration-300 hover:-translate-y-0.5 font-[600]',
+    active ? 'text-white' : '',
     !active ? 'text-black' : ''
   );
 
-  return <a className={className}>NavBarItem</a>;
+  return (
+    <a href={href} className={className}>
+      {text}
+    </a>
+  );
 };
