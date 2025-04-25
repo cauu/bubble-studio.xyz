5db846b912757d9b1092f5bf940372e76bbbf3ef
caiyufu <cauu@163.com>
2025-04-25 14:59:00 +0800

    feat: bubble background

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,14 @@
 import { NavBar } from '../NavBar';
+// import BubbleBackground from '../BubbleBackground';
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center comic-bg h-full w-full">
-      <div className="h-full w-full max-w-[1536px] p-6">
+    <div className="flex justify-center comic-bg h-full w-full relative">
+      {/* <BubbleBackground count={15} minSize={20} maxSize={60} glassEffect={true} zIndex={10} /> */}
+
+      <div className="h-full w-full max-w-[1536px] p-6 relative">
         <NavBar />
+
         {children}
       </div>
     </div>
