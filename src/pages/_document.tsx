f0cd2be48785611da19f11900435b71c44b9a35f
caiyufu <cauu@163.com>
2025-08-19 14:04:04 +0800

    feat: fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <meta name="description" content="Your site description" />
         <link rel="icon" href="/favicon.svg" />
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         {/* 可以添加默认样式、字体等 */}
       </Head>
       <body>
